58e0ac879130a4f3ea8602fe3b6222a9e9d13c52
Gustavo Goulart <104400522+goular7@users.noreply.github.com>
2023-08-18 17:33:07 -0300

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -22,9 +22,43 @@ docker stop "CONTAINER ID NUMBER"
 docker start "CONTAINER ID NUMBER"
 
 _____________________________________________________
-*MAPEAMENTO DE PORTAS*
+MAPEAMENTO DE PORTAS
 
 #instalar nginx
 docker run -it -p 8080:80 nginx
 (no navegador: lcoalhost:8080) Welcome to nginx!
+
+_____________________________________________________
+Instalação (ubuntu)
+
+#verificar pacotes
+sudo apt update
+
+#atualizar pacotes
+sudo apt Upgrade
+
+#instalar pacotes https
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+
+#instalar chave gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add –
+
+#adicionar repositório docker
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+# atualizar as informações do repositório 
+sudo apt-update
+
+#verificação
+apt-cache policy docker-ce
+
+# Instalação do Docker
+sudo apt install docker-ce
+
+#Verifique o serviço do docker
+sudo systemctl status docker
+
+_____________________________________________________
+Portainer
+
 
